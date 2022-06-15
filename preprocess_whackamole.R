b299805312266b60b25a0ed9e6c0f14e4d881f90
Bastian Ilso <hougaard.junior@gmail.com>
2022-06-15 14:58:14 +0200

    add calcutils and visutils

--- preprocess_whackamole.R
+++ preprocess_whackamole.R
@@ -4,10 +4,10 @@ source("utils/loadrawdata.R")
 
 options("digits.secs"=6)
 # Load data from directories
-D <- LoadFromDirectory("data")
+#D <- LoadFromDirectory("data")
 
-save(D, file = 'data_whack_raw.rda', compress=TRUE)
-#load('data_whack_raw.rda')
+#save(D, file = 'data_whack_raw.rda', compress=TRUE)
+load('data_whack_raw.rda')
 
 # Load data from Google Sheets
 L <- gsheet2tbl('https://docs.google.com/spreadsheets/d/1k0Z2F5E3WeDRdzlxepo-tuQasdhFwQRGoYVBtmXHdWA/edit#gid=237988817')
@@ -20,8 +20,36 @@ D <- D %>% rename(Condition = i2, Participant = i1)
 D <- D %>% mutate(Participant = as.numeric(Participant))
 
 D = D %>% mutate(Timestamp = as.POSIXct(Timestamp, format = "%Y-%m-%d %H:%M:%OS"),
-                 Framecount = as.integer(Framecount)) %>%
+                 Framecount = as.integer(Framecount),
+                 GameTimeSpent = as.numeric(GameTimeSpent),
+                 GameTimeLeft = as.numeric(GameTimeLeft),
+                 MoleActivatedDuration = as.numeric(MoleActivatedDuration),
+                 RightControllerPosWorldZ = as.numeric(RightControllerPosWorldZ),
+                 RightControllerPosWorldY = as.numeric(RightControllerPosWorldY),
+                 RightControllerPosWorldX = as.numeric(RightControllerPosWorldX),
+                 LeftControllerPosWorldZ = as.numeric(LeftControllerPosWorldZ),
+                 LeftControllerPosWorldY = as.numeric(LeftControllerPosWorldY),
+                 LeftControllerPosWorldX = as.numeric(LeftControllerPosWorldX),
+                 MotorSpaceCenterPositionX = as.numeric(MotorSpaceCenterPositionX),
+                 MotorSpaceCenterPositionY = as.numeric(MotorSpaceCenterPositionY),
+                 MotorSpaceCenterPositionZ = as.numeric(MotorSpaceCenterPositionZ),
+                 MotorSpaceHeight = as.numeric(MotorSpaceHeight),
+                 MotorSpaceWidth = as.numeric(MotorSpaceWidth),
+                 LeftControllerPosTravelX = ifelse(is.na(as.numeric(LeftControllerPosTravelX)), 0, as.numeric(LeftControllerPosTravelX)),
+                 LeftControllerPosTravelY = ifelse(is.na(as.numeric(LeftControllerPosTravelY)), 0, as.numeric(LeftControllerPosTravelY)),
+                 LeftControllerPosTravelZ = ifelse(is.na(as.numeric(LeftControllerPosTravelZ)), 0, as.numeric(LeftControllerPosTravelZ)),
+                 RightControllerPosTravelX = ifelse(is.na(as.numeric(RightControllerPosTravelX)), 0, as.numeric(RightControllerPosTravelX)),
+                 RightControllerPosTravelY = ifelse(is.na(as.numeric(RightControllerPosTravelY)), 0, as.numeric(RightControllerPosTravelY)),
+                 RightControllerPosTravelZ = ifelse(is.na(as.numeric(RightControllerPosTravelZ)), 0, as.numeric(RightControllerPosTravelZ)),
+                 Distance = LeftControllerPosTravelX + LeftControllerPosTravelY + LeftControllerPosTravelZ +
+                            RightControllerPosTravelX + RightControllerPosTravelY + RightControllerPosTravelZ) %>%
   arrange(Timestamp)
+
+#############
+# Count no. of movements inside/outside the motorspace
+#############
+# TODO.
+
 
 #############
 # Merge
@@ -33,4 +61,5 @@ D <- D %>% left_join(L, by=c('Condition' = 'Condition', 'Participant' = 'Partici
 #############
 # Split into 4 
 
-save(D %>% , file = 'data_whack1.rda', compress=TRUE)
+save(D, file = 'data_whack1.rda', compress=TRUE)
+
--- whackamole_analysis.R
+++ whackamole_analysis.R
@@ -1,13 +1,58 @@
 library(plotly)
 library(tidyverse)
 options("digits.secs"=6)
+source("utils/visutils.R")
+source("utils/calcutils.R")
+
 
 fig <- plot_ly() %>%
   config(scrollZoom = TRUE, displaylogo = FALSE, modeBarButtonsToRemove = c("pan2d","select2d","hoverCompareCartesian", "toggleSpikelines","zoom2d","toImage", "sendDataToCloud", "editInChartStudio", "lasso2d", "drawclosedpath", "drawopenpath", "drawline", "drawcircle", "eraseshape", "autoScale2d", "hoverClosestCartesian","toggleHover", "")) %>%
   layout(dragmode = "pan", showlegend=T, xaxis=list(mirror=T, ticks='outside', showline=T), yaxis=list(mirror=T, ticks='outside', showline=T))
 
-load('data_whack.rda')
+load('data_whack1.rda')
+
+
 
 #############
 # Summaries
 #############
+
+Sch <- D %>% filter(!Hand == "Both") %>% group_by(Participant,Condition,Motorspace) %>%
+  summarize(MoleHit = sum(Event == "Mole Hit"),
+            MoleSpawned = sum(Event == "Mole Spawned"),
+            HitRate = MoleHit / MoleSpawned * 100,
+            Speed = mean(MoleActivatedDuration, na.rm=T),
+            Distance = sum(Distance)) %>% group_by(Motorspace) %>% 
+  summarize(MoleHit = mean(MoleHit),
+            MoleSpawned = mean(MoleSpawned),
+            HitRateSD = sd(HitRate),
+            HitRate = mean(HitRate),
+            SpeedSD = sd(Speed),
+            DistanceSD = sd(Distance),
+            `Hit Rate` = paste0(format(round(HitRate,0), nsmall = 0),'\\%'," (",format(round(mean(HitRateSD),1), nsmall = 1),")"),
+            Speed = paste0(format(round(mean(Speed),2), nsmall = 2),"s"," (",format(round(mean(SpeedSD),1), nsmall = 1),")"),
+            Distance = paste0(format(round(mean(Distance),1), nsmall = 1),"m"," (",format(round(mean(DistanceSD),1), nsmall = 1),")")
+            ) %>% select(-MoleHit, -MoleSpawned, -HitRate,-HitRateSD,-SpeedSD,-DistanceSD)
+
+paste(colnames(Sch), collapse=" & ")
+message(paste(Sch %>% apply(.,1,paste,collapse=" & "), collapse=" \\\\ "))
+
+# summary of condition order
+Sco <- D %>% group_by(Motorspace, Order) %>%
+  summarize(speed = mean(MoleActivatedDuration, na.rm=T))
+
+# summary of mole
+Sm <- D %>% group_by(MoleId) %>%
+  summarize(speed = mean(MoleActivatedDuration, na.rm=T),
+            sd = sd(MoleActivatedDuration, na.rm=T))
+
+plot_ly(data = D %>% filter(Participant == 7), x=~Timestamp,
+        y=~MoleActivatedDuration, mode='markers',type='scatter')
+
+#[Yesterday 18.26] Hendrik Knoche
+#For the table. I'd remove space between number and unit. %, m, s.
+#Maybe include SDs from per person mean
+# like 1
+#[Yesterday 18.42] Hendrik Knoche
+#How many out of range events/time/m? Outside motor space?
+# like 1
